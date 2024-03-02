810ac5f1d85d334766b49e6fb050c9a4ba78e4d5
Debdeep Pal <deep64222@gmail.com>
2024-03-02 09:39:43 +0530

    Added CommonJS Support

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
-export default {
-    preset: "ts-jest",
-    testEnvironment: "node",
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
